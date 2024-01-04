d0cd36978eff3215161c4a6476ca65b0783c58b4
xlxwhy <1509868568@qq.com>
2024-01-04 19:58:05 +0800

    daily commit

--- src/stock/filter/OneExchangeFilter.js
+++ src/stock/filter/OneExchangeFilter.js
@@ -21,6 +21,37 @@ module.exports = {
         let DAYS=20
         let sum=0
         let sumi=0
+
+
+
+        sum=0
+        sumi=0
+        for (let di = DAYS/2; di <= DAYS; di++) {
+            const klines = stock.klines;
+            let close1=this.extractFieldFromKLine(klines, -di+1, "close", 0)
+            let rate1=this.extractFieldFromKLine(klines, -di+1, "exchange", 0)
+            let rate2=this.extractFieldFromKLine(klines, -di+0, "exchange", 0)
+            let rate3=this.extractFieldFromKLine(klines, -di-1, "exchange", 0)
+            let rate4=this.extractFieldFromKLine(klines, -di-2, "exchange", 0)
+            let a={}
+            a.exa1=rate1
+            a.exa11=rate2
+            a.exa12=parseInt((rate2+rate3)/2)
+            a.exa13=parseInt((rate2+rate3+rate4)/3)
+
+            a.exa_dp11=parseInt((a.exa1-a.exa11)/a.exa11*100)
+            a.exa_dp12=parseInt((a.exa1-a.exa12)/a.exa12*100)
+            a.exa_dp13=parseInt((a.exa1-a.exa13)/a.exa13*100)
+
+            if(a.exa_dp11>200 &&a.exa_dp12>200 && a.exa_dp13>200 && close1>0){
+                sum++ 
+            }
+        }
+        check&=(sum==1)
+
+
+        sum=0
+        sumi=0
         for (let di = 1; di <= DAYS; di++) {
             const klines = stock.klines;
             let close1=this.extractFieldFromKLine(klines, -di+1, "close", 0)
